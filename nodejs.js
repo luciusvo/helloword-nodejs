9967819f86347d697b777ad9969a1e4785beebcf
unknown <lucius.vo.93@gmail.com>
2015-05-26 09:35:20 +0700

    New File

---
+++ nodejs.js
@@ -0,0 +1,7 @@
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<h1>Hello!</h1><p>This is Heroku</p>");
+  response.end();
+});
+server.listen(8000);
